342847d4e2fd991be8d34ae7c90940d034fb97ca
Jerry Compton <jerry@Jerrys-MBP.fios-router.home>
2020-02-05 20:02:48 -0800

    Update and final tweaks to state control

--- calculator/MainPage.xaml.cs
+++ calculator/MainPage.xaml.cs
@@ -5,6 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+//Jerry Compton
+//CS481
+//Assignment 2 -- CALCULATOR
+//This file contains all of the logic for my calculator
+
 
 namespace calculator
 {
@@ -24,32 +29,64 @@ namespace calculator
         double first_num; //The first number in the math expression
         double second_num; // The second number in the math expression
         bool is_first_operand = true;//Initially on first operand in the math expression.
+        int count = 0;//initial count used for restricting the LENGTH of the number inputted into the calculator
 
-
-
+        
+        //is_select_operator is true if the user has already selected an operator
+        //is_first_after_select_op is true if it is the first number selected after selecting an operator
+        bool is_select_operator = false; //default to false because the user has not selected operator yet
+        bool is_first_after_select_op = true; //default to true because it is initially the first digit after selecting the operator
 
         //Handler for when a number is selected
         void on_select_num(object sender, EventArgs e)
         {
+            
             Button button = (Button)sender;
             string num = button.Text; //number pressed
 
-
-            this.resultText.Text += num; // add on to Text
-
-            double number;//the converted number
-
-            if (double.TryParse(this.resultText.Text, out number))//Convert the text to double
+            //IF THE NUMBER HAS NOT REACHED ITS MAX LENGTH
+            if(count <= 11) 
             {
-                this.resultText.Text = number.ToString("N0");
-                if (is_first_operand)
+
+                if (is_select_operator)//If the user has selected an operator
                 {
-                    first_num = number;
-                    
+                    //if it is the first digit after selecting the operator
+                    //then we must first set Text to zero
+                    if (is_first_after_select_op)
+                    {
+                        this.resultText.Text = "0";//reset to zero first
+                        this.resultText.Text += num;
+                        is_first_after_select_op = false;
+                        count++;
+
+                    }
+                    else//it is not the first digit after selecting operator, therefore just add normally
+                    {
+                        this.resultText.Text += num;
+                        count++;
+                    }
                 }
-                else
+                else //it is the first number so the Text was already ZERO
                 {
-                    second_num = number;
+                    this.resultText.Text += num; // add on to Text
+                    count++;
+                }
+                double number;//the converted number
+
+                if (double.TryParse(this.resultText.Text, out number))//Convert the text to double
+                {
+                    this.resultText.Text = number.ToString("N0");
+                    if (is_first_operand)
+                    {
+                        first_num = number;
+
+                    }
+                    else
+                    {
+                        second_num = number;
+
+                    }
+
                 }
             }
 
@@ -66,23 +103,27 @@ namespace calculator
             {
                 case "x":
                     first_num = result = first_num * second_num;
-                    this.resultText.Text = result.ToString();//Display
+                    this.resultText.Text = Math.Round(result,5).ToString();//Display /w only up to 5 decimal
                     break;
                 case "/":
                     first_num = result = first_num / second_num;
-                    this.resultText.Text = result.ToString();//Display
+                    this.resultText.Text = Math.Round(result,5).ToString();//Display w/ only up to 5 decimal
                     break;
                 case "+":
                     first_num = result = first_num + second_num;
-                    this.resultText.Text = result.ToString();//Display
+                    this.resultText.Text = Math.Round(result, 5).ToString();//Display w/ only up to 5 decimal
                     break;
                 case "-":
                     first_num = result = first_num - second_num;
-                    this.resultText.Text = result.ToString();//Display
+                    this.resultText.Text = Math.Round(result, 5).ToString();//Display w/ only up to 5 decimal
 
                     break;
             }
+            
             is_first_operand = false; //Allow to continue operating on result
+            count = 0;//reset count (length of input)
+            is_select_operator = false; //reset to default
+            is_first_after_select_op = true; //reset to default
         }
 
 
@@ -93,7 +134,9 @@ namespace calculator
             Button button = (Button)sender;
             math_operator = button.Text; //extract the operator
             is_first_operand = false; //now we need the second operand
-            this.resultText.Text = "0"; //i default this back to zero before the user inputs the next number
+            //this.resultText.Text = "0"; //i default this back to zero before the user inputs the next number
+            count = 0; //reset count
+            is_select_operator = true; //the select operator has been pressed -> true
         }
 
 
@@ -105,7 +148,10 @@ namespace calculator
             this.resultText.Text = "0";
             first_num = 0;
             second_num = 0;
-            is_first_operand = true;
+            is_first_operand = true; //reset to default
+            count = 0;//reset count
+            is_select_operator = false; //reset to default
+            is_first_after_select_op = true; //reset to default
 
         }
 
